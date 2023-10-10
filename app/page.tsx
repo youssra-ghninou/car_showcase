f4961a324859e8da9b9c2238143b4d43ed16ecd5
ussraa <92607242+youssra-ghninou@users.noreply.github.com>
2023-10-10 22:31:28 +0100

    feat: cars fetched

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import CustomFilter from '@/components/CustomFilter'
 import Hero from '@/components/Hero'
 import SearchBar from '@/components/SearchBar'
+import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const allCars= await fetchCars()
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -13,14 +17,23 @@ export default function Home() {
           <p>Explore the cars you might like</p>
           <div className="home__filters">
             <SearchBar />
-            <div className="home__filter-container">
+            {/* <div className="home__filter-container">
               <CustomFilter title="fuel" />
               <CustomFilter title="year" />
-            </div>
+            </div> */}
+            {!isDataEmpty ? (
+              <section>
+                We have cars
+              </section>
+            ) : (
+              <div className='home__error-container'>
+                <h2 className='text-black text-xl font-bold'>Oooops no results</h2>
+                <p> {allCars?.message}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      
     </main>
   )
 }
